9d31e49dee2b4d241fb64c636078312d01e55298
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-03-14 11:08:56 +0500

    chore: workflow fix for deployment

--- flake.nix
+++ flake.nix
@@ -6,32 +6,43 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... } @ inputs:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ ];
-          config.allowUnfree = true;
-        };
-      in
-      {
-        # Nix script formatter
-        formatter = pkgs.alejandra;
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [];
+        config.allowUnfree = true;
+      };
+    in {
+      # Nix script formatter
+      formatter = pkgs.alejandra;
 
-        devShells.default = pkgs.mkShell rec {
-          name = "web-darwin";
+      # Output compiled website
+      # nix build ".?submodules=1#" -L
+      packages.default = pkgs.runCommand "public" {} ''
+        cd ${./.}
+        ${pkgs.lib.getExe pkgs.zola} build --drafts -o $out
+      '';
 
-          packages = with pkgs; [
-            # Zola
-            zola
+      # Development environment
+      devShells.default = pkgs.mkShell rec {
+        name = "web-darwin";
 
-            # Hail Nix
-            nixd
-            alejandra
-            statix
-            deadnix
-          ];
-        };
-      });
+        packages = with pkgs; [
+          # Zola
+          zola
+
+          # Hail Nix
+          nixd
+          alejandra
+          statix
+          deadnix
+        ];
+      };
+    });
 }
